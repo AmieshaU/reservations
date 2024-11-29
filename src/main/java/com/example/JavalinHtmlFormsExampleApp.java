4bb6c2d1063661a063ebb5ee3dcd8490c7ec9f2b
AmieshaU <AmieshaU@outlook.com>
2024-11-28 18:11:06 -0600

    Add files via upload

---
+++ src/main/java/com/example/JavalinHtmlFormsExampleApp.java
@@ -0,0 +1,49 @@
+package com.example;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.util.FileUtil;
+
+public class JavalinHtmlFormsExampleApp {
+
+    private static final Map<String, String> reservations = new HashMap<>() {{
+        put("saturday", "No reservation");
+        put("sunday", "No reservation");
+    }};
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public", Location.CLASSPATH);
+        });
+        app.start(7000);
+        app.post("/make-reservation", ctx -> {
+            reservations.put(ctx.formParam("day"), ctx.formParam("time"));
+            String text = new description().createDescription(ctx.formParam("day"), ctx.formParam("time"));
+
+            System.out.println("Text: " + text);
+            if (text != null) {
+                ctx.html( text);
+            } else {
+                ctx.html("No text provided");
+            }
+        });
+
+        app.get("/check-reservation", ctx -> {
+            ctx.html(reservations.get(ctx.queryParam("day")));
+        });
+
+        app.post("/upload-example", ctx -> {
+            ctx.uploadedFiles("files").forEach(file -> {
+                FileUtil.streamToFile(file.content(), "upload/" + file.filename());
+            });
+            ctx.html("Upload successful");
+        });
+        
+        app.start();
+
+    }
+
+}
---
+++ src/main/java/com/example/description.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class description {
+    public String createDescription(String day, String time) {
+        return "Your reservation at " + day + " and " + time + " is saved.";
+    }
+}
